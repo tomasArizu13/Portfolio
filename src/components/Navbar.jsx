97e1bb751096e467dfe38d62e44bc8e78291d183
tomasArizu13 <tomasarizu13@gmail.com>
2024-02-20 12:48:11 -0300

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,7 +28,7 @@ const Navbar = () => {
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
             Tomas Arizu &nbsp; <span 
-            className='sm:block hidden'> | Junior</span>
+            className='sm:block hidden'> | Portfolio</span>
           </p>
         </Link>
         <ul className='list-none hidden sm:flex flex-row gap-10'>
